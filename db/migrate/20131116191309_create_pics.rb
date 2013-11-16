97b05b5f85b2bdf919b26f4fcdeaee8e4c12f8b9
shaheen16 <shaheen16@shaheen16-VirtualBox.(none)>
2013-11-16 14:29:33 -0500

    the model has been created

---
+++ db/migrate/20131116191309_create_pics.rb
@@ -0,0 +1,10 @@
+class CreatePics < ActiveRecord::Migration
+  def change
+    create_table :pics do |t|
+    	t.string :emotion
+    	t.text 	 :learned
+    	t.integer :lesson
+      t.timestamps
+    end
+  end
+end
