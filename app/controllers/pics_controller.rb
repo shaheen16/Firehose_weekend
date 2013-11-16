875503130ef55efc9fef1c3b78e582fbc930585b
shaheen16 <shaheen16@shaheen16-VirtualBox.(none)>
2013-11-16 16:18:39 -0500

    new code

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,4 +2,19 @@ class PicsController < ApplicationController
 	def index
 		@pics = Pic.all
 	end
-end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.create(pic_params)
+		redirect_to pics_path
+	end
+
+	private 
+	def pic_params
+		params.require(:pic).permit(:lesson, :emotion, :learned)
+	end 
+		
+	end
